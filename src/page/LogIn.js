2d41fed7d41b9fcf50514e623d245db990f9509e
TsiaYu1304 <openopen290@gmail.com>
2021-12-24 20:57:49 +0800

    style-loginPage

--- src/page/LogIn.js
+++ src/page/LogIn.js
@@ -1,10 +1,33 @@
+import img_login from "../assests/landing/login.png";
+import { Link } from 'react-router-dom';
+
 function LogIn() {
-    return (
-        
-       <p>登入頁面</p>
-       
-        
-    );
-  }
-  
-  export default LogIn;
+   return (
+
+      <div className="login_container">
+         <div className="login_image">
+            <img alt="" className="login_image_img" src={img_login} />
+         </div>
+         <div className="login_input">
+            <form className="login_form">
+               <p className="font_24">歡迎回到Nstar!</p>
+               <p className="font_20 mt_48">電子郵件</p>
+               <p><input className="input mt_16" type="text" name="userName" placeholder="輸入您的電子郵件" required /></p>
+               <p className="font_20 mt_48">密碼</p>
+               <p><input className="input mt_16" type="password" name="userName" placeholder="輸入您的密碼" required /></p>
+               <Link to={"/"}>
+               <div className="btn_selected mt_48">登入</div>
+               </Link>
+               <div className="display_flex mt_16">
+               <p>還沒有帳號？</p>
+               <Link to={"/signup"}>
+               <p className="font_gray">點我註冊</p>
+               </Link>
+               </div>
+            </form>
+         </div>
+      </div>
+   );
+}
+
+export default LogIn;
