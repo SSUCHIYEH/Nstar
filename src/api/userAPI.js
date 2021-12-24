4ff3a5ad3b70b23e6dfcf103703b97913f7280bb
Ruby Yeh <hyoka4654@gmail.com>
2021-12-25 01:04:23 +0800

    [fix] productDetail頁錯誤

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -3,7 +3,7 @@ const baseURL = "https://nstar-backend.herokuapp.com"
 
 export const signInWithEmailPassword = async (email, password) => {
     try {
-      let res = await axios.post(`${URL}/users/signin`, { email, password });
+      let res = await axios.post(`${URL}/users/user/signin`, { email, password });
       return { status: res.status, user: res.data };
     } catch (err) {
       return { status: err.response.status, detail: err.response.data.detail };
--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,5 +1,3 @@
-import { useContext } from "react";
-import { StoreContext } from "../store";
 import AddToCart from "../component/AddToCart";
 import chat from '../assests/Icon/chat-bubble.png';
 import like from '../assests/Icon/like.png'
@@ -7,46 +5,43 @@ import Productlist from "../component/Productlist";
 import anotherProduct from '../json/women_bottom.json';
 
 export default function ProductDetail() {
-    const { state: { productDetail: { product } } } = useContext(StoreContext);
-
     return (
         <>
             <div className="productdetail_container">
                 <div className="productdetail_img">
-                    <img alt="" className="productdetail_img_img" src={product.imgUrl_1} />
+                    <img alt="" className="productdetail_img_img" src="" />
                 </div>
                 <div className="productdetail_detail">
                     <div className="productdetail_detail_detail">
                         <div className="productdetail_seller">
-                            <img className="productdetail_sellericon" src={product.imgUrl_1} alt="" />
-                            <div className="productdetail_sellername">{product.seller_name}</div>
+                            <img className="productdetail_sellericon" src="" alt="" />
+                            <div className="productdetail_sellername">Becca</div>
                         </div>
-                        <div className="productdetail_title">{product.name}</div>
-                        <div className="productdetail_price">NT {product.price}</div>
+                        <div className="productdetail_title">好看衣服</div>
+                        <div className="productdetail_price">NT 200</div>
                         <div className="productdetail_button">
                             <img className="productdetail_icon" src={like} alt="" />
                             <img className="productdetail_icon" src={chat} alt="" />
                         </div>
-                        <AddToCart />
                         <div className="productdetail_information">
                             <div className="productdetail_item">尺寸</div>
-                            <div className="productdetail_answer">{product.size}</div>
+                            <div className="productdetail_answer">M</div>
                         </div>
                         <div className="productdetail_information">
                             <div className="productdetail_item">品牌</div>
-                            <div className="productdetail_answer">{product.brand}</div>
+                            <div className="productdetail_answer">無</div>
                         </div>
                         <div className="productdetail_information">
                             <div className="productdetail_item">狀況</div>
-                            <div className="productdetail_answer">{product.content}</div>
+                            <div className="productdetail_answer">讚</div>
                         </div>
                         <div className="productdetail_information">
                             <div className="productdetail_item">顏色</div>
-                            <div className="productdetail_answer">{product.color}</div>
+                            <div className="productdetail_answer">黃色</div>
                         </div>
                         <div className="productdetail_information">
                             <div className="productdetail_item">標籤</div>
-                            <div className="productdetail_answer">{product.tag}</div>
+                            <div className="productdetail_answer">男友風</div>
                         </div>
 
 
