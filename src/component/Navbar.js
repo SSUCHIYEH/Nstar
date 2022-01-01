e6d15ce82f9b522ab28a047b4e60acbc1ef44891
TsiaYu1304 <openopen290@gmail.com>
2022-01-01 16:04:04 +0800

    fix - header RWD

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import NavItem from "./NavItem";
 import { StoreContext } from "../store"
 import { Badge } from "antd";
-import landind_page_img from '../assests/Icon/logo.png'
+import landind_page_img from '../assests/Icon/logo.png';
+import img_bars from "../assests/Icon/bars.png";
 import search_icon from "../assests/Icon/loupe.png"
 import like_icon from '../assests/Icon/like.png'
 import account_icon from '../assests/Icon/account.png'
@@ -13,6 +14,7 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 
 
+
 export default function NavBar() {
     const history = useHistory();
     const dispatch = useDispatch();
@@ -20,6 +22,12 @@ export default function NavBar() {
     //const { state: { cartItems } } = useContext(StoreContext);
     const [active, setAcitve] = useState(false);
     const [ClassName, setClassname] = useState("collapse hide");
+
+    //漢堡選單
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+    const toggleHamburger = () => {
+        setHamburgerOpen(!hamburgerOpen)
+    }
     // let counts = cartItems.length;
 
     function toggle() {
@@ -48,6 +56,7 @@ export default function NavBar() {
 
 
     return (
+
         <div className="Header">
             <nav className="navbar" role='navigation'>
                 <ul className="ul nav_list">
@@ -113,10 +122,138 @@ export default function NavBar() {
                             </>
                     }
                 </ul>
+                <ul className="hamburger hamburger_p" onClick={toggleHamburger}>
+                    <img alt="" className="img_26" src={img_bars} />
+                </ul>
+
 
             </nav>
 
-            {/* {
+
+            {/* 375的header */}
+            <nav className="nav_375" role='navigation'>
+                <ul className="display_center_between">
+
+                    <li>
+                        <NavItem to="/" >
+                            <img alt="" className="img_logoSmall" src={landind_page_img} />
+                        </NavItem>
+
+                    </li>
+                    <li className="display_flex">
+                        <li className="">
+                            <img alt="" className="img_26" src={search_icon} />
+                        </li>
+                        <li className="ml_24">
+                            <div onMouseEnter={toggle}>
+                                <NavItem to="/usercart">
+                                    <img alt="" className="img_26" src={cart_icon} />
+                                </NavItem>
+                            </div>
+                        </li>
+                        <li className="ml_24">
+                            <img alt="" className="img_26" src={img_bars} onClick={toggleHamburger} />
+                        </li>
+                    </li>
+                </ul>
+                {hamburgerOpen ?
+                    <ul className="mt_36 h_full">
+                        <li>
+                            <p className="font_20 font_weight_500">women</p>
+                            <li className="w_180 mt_16 ml_36">
+                                <li onClick={toggleHamburger}>
+                                    <NavItem to="/product/category/women_top" ><p className="font_black">top</p></NavItem>
+                                </li>
+                                <div className="line mt_8"></div>
+                            </li>
+                            <li className="w_180 mt_16 ml_36">
+                                <li onClick={toggleHamburger}>
+                                    <NavItem to="/product/category/women_bottom" ><p className="font_black">bottom</p></NavItem>
+                                </li>
+                                <div className="line mt_8"></div>
+                            </li>
+                        </li>
+                        <li className="mt_36">
+                            <p className="font_20 font_weight_500">men</p>
+                            <li className="w_180 mt_16 ml_36">
+                                <li className="" onClick={toggleHamburger} >
+                                    <NavItem to="/product/category/men_top" ><p className="font_black">top</p></NavItem>
+                                </li>
+                                <div className="line mt_8"></div>
+                            </li>
+                            <li className="w_180 mt_16 ml_36">
+                                <li onClick={toggleHamburger}>
+                                    <NavItem to="/product/category/men_bottom" ><p className="font_black">bottom</p></NavItem>
+                                </li>
+                                <div className="line mt_8"></div>
+                            </li>
+                        </li>
+                        {userInfo ?
+                            <li className="mt_36 ">
+                                <li className="font_20 font_weight_500">我的帳號</li>
+                                <li className="w_180 mt_16 ml_36">
+                                    <li onClick={toggleHamburger}>
+                                        <NavItem to="/userproductshop" ><p className="font_black">我的商店</p></NavItem>
+                                    </li>
+                                    <div className="line mt_8"></div>
+                                </li>
+                                <li className="w_180 mt_16 ml_36">
+                                    <li onClick={toggleHamburger}>
+                                        <NavItem to="/userorderquery" ><p className="font_black">查看購買紀錄</p></NavItem>
+                                    </li>
+                                    <div className="line mt_8"></div>
+                                </li>
+                                <li className="w_180 mt_16 ml_36">
+                                    <li onClick={toggleHamburger}>
+                                        <NavItem to="/" ><p className="font_black">我的收藏</p></NavItem>
+                                    </li>
+                                    <div className="line mt_8"></div>
+                                </li>
+                                <li className="w_180 mt_16 ml_36">
+                                    <li onClick={toggleHamburger}>
+                                        <NavItem to="/" ><p className="font_black">帳號設定</p></NavItem>
+                                    </li>
+                                    <div className="line mt_8"></div>
+                                </li>
+                                <li className="w_180 mt_16 ml_36">
+                                    <li onClick={toggleHamburger}>
+                                        <a to="/" onClick={hangleLogout} ><p className="font_black">登出</p></a>
+                                    </li>
+                                    <div className="line mt_8"></div>
+                                </li>
+                                
+                            </li>
+                            :
+                            <div className="display_center_between ml_36 mt_48 w_230">
+                            <li>
+                                <button className="btn_unSelected">
+                                    <a href="/login" className="font_black font_hover_y">Log in</a>
+                                    </button>
+                            </li>
+                            <li>
+                            <button className="btn_selected">
+                                <a href="/login" className="font_white">Sign up</a>
+                                </button>
+                            </li>
+
+                        </div>
+
+                            }
+                    </ul>
+                    :
+                    ""
+                }
+
+            </nav>
+
+        </div>
+    )
+
+}
+
+
+
+{/* {
                 counts > 0 ?
                     <div onMouseLeave={toggle} className={ClassName}>
                         {cartItems.map(product => (
@@ -150,7 +287,3 @@ export default function NavBar() {
                     </div>
 
             } */}
-        </div>
-    )
-
-}
