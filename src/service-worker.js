f50a140be06170d5b8226219c7ce82cd6dfdb3b3
TsiaYu1304 <openopen290@gmail.com>
2022-01-02 17:31:06 +0800

    feature- add catch api image

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,7 +11,7 @@ import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
 
 clientsClaim();
 
@@ -49,14 +49,29 @@ registerRoute(
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
-  // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
-    cacheName: 'images',
+  new RegExp(".(png|svg|jpg|jpeg|ico)$"),
+  new CacheFirst({
+    cacheName: "cache-images",
     plugins: [
-      // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60,
+        maxEntries: 50,
+        purgeOnQuotaError: true,
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  new RegExp("https://nstar-backend.herokuapp.com/api/v1/products"),
+  new StaleWhileRevalidate({
+    cacheName: "cache-productsAPI",
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60,
+        maxEntries: 50,
+        purgeOnQuotaError: true,
+      }),
     ],
   })
 );
